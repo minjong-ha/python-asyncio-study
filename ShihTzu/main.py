595402bd249d89d1e8ed6140039b0636dcfa1ae4
minjong-ha <hmjong1128@gmail.com>
2022-12-22 10:47:07 +0900

    feat(dbus): Add common interface managing all objects

--- ShihTzu/main.py
+++ ShihTzu/main.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 
-from ShihTzu.modules.controller import dbus_publish
+from ShihTzu.modules.controller import CageController, dbus_publish
 
 
 from .utils import logger
@@ -9,16 +9,6 @@ from .modules import *
 from .modules.models.shihtzu import ShihTzu
 
 _logger = logger.get_logger(os.path.basename(__file__))
-
-async def print_log_every_n_sec(_arg: str, _interval: int):
-    while True:
-        _logger.info(f"{_arg} for every {_interval} seconds")
-        await asyncio.sleep(_interval)
-
-
-async def return_value_async():
-    return True
-
 
 def _handle_exception(self, loop, context):
     _logger.error(f"Exception: {context}")
@@ -32,20 +22,32 @@ def _handle_exception(self, loop, context):
 
 def run():
     loop = asyncio.get_event_loop() 
-    loop.set_exception_handler(_handle_exception)
+    #loop.set_exception_handler(_handle_exception)
+
+    # Create cage controller rule them all
+    cage_controller = CageController(loop)
 
     # Create ShihTzu objects and make them run()
+    puppies = list()
+
     poppi = ShihTzu("poppi", 12)
     toto = ShihTzu("toto", 10)
+    tori = ShihTzu("tori", 3)
 
-    loop.create_task(toto.run())
-    loop.create_task(poppi.run())
+    puppies.append(poppi)
+    puppies.append(toto)
+    puppies.append(tori)
 
-    # Assign dbus-interface for ShihTzu objects
+    # Assign dbus-interface for ShihTzu objects and cage
     controllers = list()
-    controllers.append(toto.controller)
-    controllers.append(poppi.controller)
+    controllers.append(cage_controller)
+    for puppy in puppies:
+        controllers.append(puppy.controller)
 
     loop.create_task(dbus_publish(controllers))
+
+    for puppy in puppies:
+        loop.create_task(puppy.run())
+        loop.create_task(puppy.controller.initialize())
 
     loop.run_forever()
--- ShihTzu/modules/models/shihtzu.py
+++ ShihTzu/modules/models/shihtzu.py
@@ -8,41 +8,56 @@ from ...utils import logger
 _logger = logger.get_logger(os.path.basename(__file__))
 
 DEFAULT_HUNGER_INTERVAL = 10
+RANDOM_INTERVAL = 30
+MAX_STATE = 100
+MAX_HUNGER = 100
 
 class ShihTzu:
     def __init__(self, _name, _age):
         self.name = _name
         self.age = _age
         self.species = "Shih-Tzu"
-        self.state = 0
-        self.hunger = 0
+        self.state = MAX_STATE
+        self.hunger = MAX_HUNGER
 
         self.controller = PuppyController(self)
 
         self.exit_code = None
 
-    async def random_bark(self):
+    async def _bark(self):
+        _logger.info(f"{self.name}({self.age}) Bark! - state: {self.state}  hunger: {self.hunger}")
+
+    async def _default_state_decrease(self, interval):
         while True:
-            _random = random.randint(1,10)
-            await asyncio.sleep(_random)
-            _logger.info(f"{self.name}:{self.age} Bark! for {_random} sec")
+            await asyncio.sleep(interval)
+            self.state -= 1
+            #_logger.info(f"{self.name}({self.age})'s state: {self.state}")
+
+            if (random.randrange(100) < (100 - self.state)):
+                await self._bark()
+
 
     async def _default_hunger_decrease(self, interval):
         while True:
             await asyncio.sleep(interval)
-            self.hunger += 1
-            _logger.info(f"{self.name}({self.age})'s hunger: {self.hunger}")
+            self.hunger -= 1
+            #_logger.info(f"{self.name}({self.age})'s hunger: {self.hunger}")
+
+            if (random.randrange(100) < (100 - self.state)):
+                await self._bark()
+
+
+    # TODO (minjong, 221220): Add get_properties via dbus-interface
+    # TODO (minjong, 221220): Make state and hunger observable. If it becomes 0, puppy will die...
+    # TODO (minjong, 221220): Add on_signal_cb for Oneshot DBUS Interface
 
     async def prepare(self, _name: str, _age: int):
-        # assign dbus-interface(user) for socket income
 
-        asyncio.create_task(self.random_bark())
         asyncio.create_task(self._default_hunger_decrease(DEFAULT_HUNGER_INTERVAL))
-        #asyncio.create_task(dbus_publish(self._interact_dbus_interface))
+        asyncio.create_task(self._default_state_decrease(DEFAULT_HUNGER_INTERVAL))
 
         _logger.info(f"{self.name}({self.age}) prepare()")
 
-        # Never reached since random_bark()
         return True
 
     async def run(self):
